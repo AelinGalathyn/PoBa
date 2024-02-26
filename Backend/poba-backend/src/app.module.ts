47ac06edd4bdeda84dc854691eeecd0962e4860b
AelinGalathyn <szandra.szabina9@gmail.com>
2024-02-26 16:03:52 +0100

    TypeOrmModule import

--- Backend/poba-backend/src/app.module.ts
+++ Backend/poba-backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql', // type of our database
+    host: 'poba-project-server.mysql.database.azure.com', // database host
+    port: 3306, // database host port
+    username: 'izizvdeqos', // username
+    password: 'SYW8433LFY7EPV7K$', // user password
+    database: 'poba-project-database', // name of our database,
+    entities: [], // entities go here
+    synchronize: true, // if true, TypeORM will automatically create the database schema based on your models
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
