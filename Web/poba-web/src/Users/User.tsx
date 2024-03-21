a23cbed650bb25e1ade44104fdf88d54ea00d740
Pál Csenge <114151999+Butter3eer@users.noreply.github.com>
2024-03-21 11:46:18 +0100

    workspace changes

--- Web/poba-web/src/Users/User.tsx
+++ Web/poba-web/src/Users/User.tsx
@@ -1,6 +1,7 @@
 export default class User {
     username : string;
     password : string;
+
     constructor(username: string, password : string) {
         this.username = username;
         this.password = password;
